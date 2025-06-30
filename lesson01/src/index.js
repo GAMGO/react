bd9e05932f1944cf2beda11ffa6f3f0d5f571f02
GAMGO <gamgo445@gmail.com>
2025-06-30 14:11:52 +0900

    아 맞다 메세지 쓰는 거 깜빡했어요

---
+++ lesson01/src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import App2 from './component/App2';
+import reportWebVitals from './reportWebVitals';
+
+//import 는 외부 파일의 함수, 라이브러리 사용할 때 미리 가져오기 (메모리 로드)
+
+//App은 화면을 구성하는 컴포넌트 이름.
+const root = ReactDOM.createRoot(document.getElementById('root'));
+//앱 컴포넌트에서 정의한 요소들을 createElement, appendChild 실행 => root,rebder(). 리액트 최신버전은 React.StrinctMode를 사용(개발과정 오류체크)
+root.render(
+  <React.StrictMode>
+    {/* <App /> */}
+    <App2 />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// 화면에서 확인하기 전에 컴포넌트 테스트가 별도로 함수로 구현.
+reportWebVitals();
