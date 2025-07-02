18266ef8b9176f89effeca09680e845ea33b71b3
GAMGO <gamgo445@gmail.com>
2025-07-02 15:43:08 +0900

    add DATAs

--- lesson03/src/main.jsx
+++ lesson03/src/main.jsx
@@ -2,11 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Appv1 from './App_v1.jsx'
 import Ar from './Arraytest.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Ar />
+    {/* <Ar /> */}
+    <Appv1 />
     {/* <App /> */}
   </StrictMode>,
 )
