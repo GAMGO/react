eb58768954a7fd717955413ea4ba368c6398b1ee
GAMGO <gamgo445@gmail.com>
2025-07-02 16:00:29 +0900

    backup

--- lesson03/src/App_V1.jsx
+++ lesson03/src/App_V1.jsx
@@ -75,10 +75,9 @@ function App() {
   };
 
   const handleSubmit = (e) => {
-    e.preventDefault(); // form 제출 기본 동작을 못하게 막음.
+    e.preventDefault(); // form 서버제출 기본 동작을 못하게 막음.
     // 입력값을 할일 목록(배열)에 추가시키는 함수 실행하기
     handleInsert(value);
-    //   ㄴ 부모 컴포넌트에서 프롭으로 받음.
     setValue("");
   };
 
@@ -96,12 +95,9 @@ function App() {
           {/* {children} */}
           {/* insert */}
           <form className="TodoInsert" onSubmit={handleSubmit}>
-            <input
-              placeholder="할 일을 입력하세요."
-              value={value}
-              onChange={(e) => setValue(e.target.value)}
-            />
-            {/* 버튼 클릭은 onSubmit => handleSubmit 함수 실행 */}
+            <input placeholder="할 일을 입력하세요." value={value} onChange={(e) => setValue(e.target.value)}/>
+            {/* type="submit"버튼 클릭은 onSubmit 이벤트 발생 => form태그에서 함수 지정(handleSubmit 함수 실행)
+            type="button"이면 onClick 이벤트 발생*/}
             <button type="submit">
               <MdAdd />
             </button>
